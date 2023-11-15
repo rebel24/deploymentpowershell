56cad214fe160efb9823bdac303be3f2fa22b006
rebel24 <jamietees@gmail.com>
2023-11-15 11:37:51 +0000

    Update provisionSQLdisks.ps1

--- provisionSQLdisks.ps1
+++ provisionSQLdisks.ps1
@@ -5,16 +5,16 @@
 $DataDisk = Get-Disk | Where-Object { $_.Number -eq 1 }
 if ($DataDisk -and $DataDisk.PartitionStyle -eq 'RAW') {
     Initialize-Disk -Number $DataDisk.Number -PartitionStyle MBR
-    $DataPartition = New-Partition -DiskNumber $DataDisk.Number -UseMaximumSize -AssignDriveLetter -DriveLetter F
-    Format-Volume -Partition $DataPartition -FileSystem NTFS -NewFileSystemLabel 'Data' -Confirm:$false
+    $DataPartition = New-Partition -DiskNumber $DataDisk.Number -UseMaximumSize -AssignDriveLetter
+    Format-Volume -DriveLetter F -Partition $DataPartition -FileSystem NTFS -NewFileSystemLabel 'Data' -Confirm:$false
 }
 
 # Initialize and format log disk
 $LogDisk = Get-Disk | Where-Object { $_.Number -eq 2 }
 if ($LogDisk -and $LogDisk.PartitionStyle -eq 'RAW') {
     Initialize-Disk -Number $LogDisk.Number -PartitionStyle MBR
-    $LogPartition = New-Partition -DiskNumber $LogDisk.Number -UseMaximumSize -AssignDriveLetter -DriveLetter G
-    Format-Volume -Partition $LogPartition -FileSystem NTFS -NewFileSystemLabel 'Log' -Confirm:$false
+    $LogPartition = New-Partition -DiskNumber $LogDisk.Number -UseMaximumSize -AssignDriveLetter
+    Format-Volume -DriveLetter G -Partition $LogPartition -FileSystem NTFS -NewFileSystemLabel 'Log' -Confirm:$false
 }
 
 # Initialize and format tempDb disk
@@ -22,6 +22,6 @@ $TempDbDisk = Get-Disk | Where-Object { $_.Number -eq 3 }
 if ($TempDbDisk -and $TempDbDisk.PartitionStyle -eq 'RAW') {
     Initialize-Disk -Number $TempDbDisk.Number -PartitionStyle MBR
     # Specify the drive letter directly in the New-Partition cmdlet
-    $TempDbPartition = New-Partition -DiskNumber $TempDbDisk.Number -UseMaximumSize -AssignDriveLetter -DriveLetter E
-    Format-Volume -Partition $TempDbPartition -FileSystem NTFS -NewFileSystemLabel 'TempDb' -Confirm:$false
+    $TempDbPartition = New-Partition -DiskNumber $TempDbDisk.Number -UseMaximumSize -AssignDriveLetter
+    Format-Volume -DriveLetter E -Partition $TempDbPartition -FileSystem NTFS -NewFileSystemLabel 'TempDb' -Confirm:$false
 }
