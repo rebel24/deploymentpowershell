17a69ff2abfdb7a5f66eea5a04e38d59a51d1470
rebel24 <jamietees@gmail.com>
2023-11-15 11:58:49 +0000

    Update provisionSQLdisks.ps1

--- provisionSQLdisks.ps1
+++ provisionSQLdisks.ps1
@@ -1,13 +1,22 @@
 # Disk initialization and formatting script
 # Modify as needed to suit your environment
 
+# Parameters
+$dataPath = 'E:\sqlData'
+$logPath = 'F:\sqlLog'
+$tempDbPath = 'G:\sqlTempDb'
+
 # Initialize and format data disk
 $DataDisk = Get-Disk | Where-Object { $_.Number -eq 1 }
 if ($DataDisk -and $DataDisk.PartitionStyle -eq 'RAW') {
     Initialize-Disk -Number $DataDisk.Number -PartitionStyle MBR
     $DataPartition = New-Partition -DiskNumber $DataDisk.Number -UseMaximumSize -AssignDriveLetter
     Format-Volume -Partition $DataPartition -FileSystem NTFS -NewFileSystemLabel 'Data' -Confirm:$false
+    
+    # Create data folder
+New-Item -ItemType Directory -Path $dataPath -Force
 }
+
 
 # Initialize and format log disk
 $LogDisk = Get-Disk | Where-Object { $_.Number -eq 2 }
@@ -15,13 +24,17 @@ if ($LogDisk -and $LogDisk.PartitionStyle -eq 'RAW') {
     Initialize-Disk -Number $LogDisk.Number -PartitionStyle MBR
     $LogPartition = New-Partition -DiskNumber $LogDisk.Number -UseMaximumSize -AssignDriveLetter
     Format-Volume -Partition $LogPartition -FileSystem NTFS -NewFileSystemLabel 'Log' -Confirm:$false
+    # Create log folder
+New-Item -ItemType Directory -Path $logPath -Force
 }
+
 
 # Initialize and format tempDb disk
 $TempDbDisk = Get-Disk | Where-Object { $_.Number -eq 3 }
 if ($TempDbDisk -and $TempDbDisk.PartitionStyle -eq 'RAW') {
     Initialize-Disk -Number $TempDbDisk.Number -PartitionStyle MBR
-    # Specify the drive letter directly in the New-Partition cmdlet
     $TempDbPartition = New-Partition -DiskNumber $TempDbDisk.Number -UseMaximumSize -AssignDriveLetter
     Format-Volume -Partition $TempDbPartition -FileSystem NTFS -NewFileSystemLabel 'TempDb' -Confirm:$false
+    # Create tempDb folder
+New-Item -ItemType Directory -Path $tempDbPath -Force
 }
