13524fb523cddc9528deebaf5173caf40482de6c
rebel24 <jamietees@gmail.com>
2023-11-15 10:00:52 +0000

    Update provisionSQLdisks.ps1

--- provisionSQLdisks.ps1
+++ provisionSQLdisks.ps1
@@ -5,7 +5,7 @@
 $DataDisk = Get-Disk | Where-Object { $_.Number -eq 0 }
 if ($DataDisk -and $DataDisk.PartitionStyle -eq 'RAW') {
     Initialize-Disk -Number $DataDisk.Number -PartitionStyle MBR
-    $DataPartition = New-Partition -DiskNumber $DataDisk.Number -UseMaximumSize -AssignDriveLetter
+    $DataPartition = New-Partition -DiskNumber $DataDisk.Number -UseMaximumSize -AssignDriveLetter -DriveLetter 'F'
     Format-Volume -Partition $DataPartition -FileSystem NTFS -NewFileSystemLabel 'Data' -Confirm:$false
 }
 
@@ -13,7 +13,7 @@ if ($DataDisk -and $DataDisk.PartitionStyle -eq 'RAW') {
 $LogDisk = Get-Disk | Where-Object { $_.Number -eq 1 }
 if ($LogDisk -and $LogDisk.PartitionStyle -eq 'RAW') {
     Initialize-Disk -Number $LogDisk.Number -PartitionStyle MBR
-    $LogPartition = New-Partition -DiskNumber $LogDisk.Number -UseMaximumSize -AssignDriveLetter
+    $LogPartition = New-Partition -DiskNumber $LogDisk.Number -UseMaximumSize -AssignDriveLetter -DriveLetter 'G'
     Format-Volume -Partition $LogPartition -FileSystem NTFS -NewFileSystemLabel 'Log' -Confirm:$false
 }
 
@@ -21,6 +21,7 @@ if ($LogDisk -and $LogDisk.PartitionStyle -eq 'RAW') {
 $TempDbDisk = Get-Disk | Where-Object { $_.Number -eq 2 }
 if ($TempDbDisk -and $TempDbDisk.PartitionStyle -eq 'RAW') {
     Initialize-Disk -Number $TempDbDisk.Number -PartitionStyle MBR
-    $TempDbPartition = New-Partition -DiskNumber $TempDbDisk.Number -UseMaximumSize -AssignDriveLetter
+    # Specify the drive letter directly in the New-Partition cmdlet
+    $TempDbPartition = New-Partition -DiskNumber $TempDbDisk.Number -UseMaximumSize -AssignDriveLetter -DriveLetter 'E'
     Format-Volume -Partition $TempDbPartition -FileSystem NTFS -NewFileSystemLabel 'TempDb' -Confirm:$false
 }
