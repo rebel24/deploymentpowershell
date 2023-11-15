fe7507962e2e30056e6a6e6ade6a433dd2f5e55a
rebel24 <jamietees@gmail.com>
2023-11-15 11:39:55 +0000

    Update provisionSQLdisks.ps1

--- provisionSQLdisks.ps1
+++ provisionSQLdisks.ps1
@@ -6,7 +6,7 @@ $DataDisk = Get-Disk | Where-Object { $_.Number -eq 1 }
 if ($DataDisk -and $DataDisk.PartitionStyle -eq 'RAW') {
     Initialize-Disk -Number $DataDisk.Number -PartitionStyle MBR
     $DataPartition = New-Partition -DiskNumber $DataDisk.Number -UseMaximumSize -AssignDriveLetter
-    Format-Volume -DriveLetter F -Partition $DataPartition -FileSystem NTFS -NewFileSystemLabel 'Data' -Confirm:$false
+    Format-Volume -Partition $DataPartition -FileSystem NTFS -NewFileSystemLabel 'Data' -Confirm:$false
 }
 
 # Initialize and format log disk
@@ -14,7 +14,7 @@ $LogDisk = Get-Disk | Where-Object { $_.Number -eq 2 }
 if ($LogDisk -and $LogDisk.PartitionStyle -eq 'RAW') {
     Initialize-Disk -Number $LogDisk.Number -PartitionStyle MBR
     $LogPartition = New-Partition -DiskNumber $LogDisk.Number -UseMaximumSize -AssignDriveLetter
-    Format-Volume -DriveLetter G -Partition $LogPartition -FileSystem NTFS -NewFileSystemLabel 'Log' -Confirm:$false
+    Format-Volume -Partition $LogPartition -FileSystem NTFS -NewFileSystemLabel 'Log' -Confirm:$false
 }
 
 # Initialize and format tempDb disk
@@ -23,5 +23,5 @@ if ($TempDbDisk -and $TempDbDisk.PartitionStyle -eq 'RAW') {
     Initialize-Disk -Number $TempDbDisk.Number -PartitionStyle MBR
     # Specify the drive letter directly in the New-Partition cmdlet
     $TempDbPartition = New-Partition -DiskNumber $TempDbDisk.Number -UseMaximumSize -AssignDriveLetter
-    Format-Volume -DriveLetter E -Partition $TempDbPartition -FileSystem NTFS -NewFileSystemLabel 'TempDb' -Confirm:$false
+    Format-Volume -Partition $TempDbPartition -FileSystem NTFS -NewFileSystemLabel 'TempDb' -Confirm:$false
 }
